5dca4e22f35b1a93f4393d55da4019b091db6505
Devin Miller <edwin0258@gmail.com>
2015-11-27 04:54:31 +0000

    Add name to destination, add more styling.

--- client/scripts/main.js
+++ client/scripts/main.js
@@ -2,11 +2,11 @@ var app = angular.module("meetupApp", []);
 
 app.controller("destinationController", function($scope){
     $scope.destinations = [];
-    $scope.newDestination = {country: '', city: '', zipcode: '', start_time: '', duration: '', description: ''}
+    $scope.newDestination = {name: '',country: '', city: '', zipcode: '', start_time: '', duration: '', description: ''}
     
     $scope.post = function(){
         
         $scope.destinations.push($scope.newDestination);
-        $scope.newDestination = {country: '', city: '', zipcode: '', start_time: '', duration: '', description: ''}
+        $scope.newDestination = {name: '',country: '', city: '', zipcode: '', start_time: '', duration: '', description: ''}
     }
 })
